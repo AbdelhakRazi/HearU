a73b47a71e40516eaea96c21b0d0cf264ceab34d
AbdelhakRazi <razi.contact3@gmail.com>
2024-04-15 22:03:57 +0200

    onboarding done

--- lib/views/onboarding/components/animated_action_buttons.dart
+++ lib/views/onboarding/components/animated_action_buttons.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hearu/config/colors.dart';
 import 'package:hearu/state/onboarding_animation/onboarding_animation_bloc.dart';
+import 'package:hearu/views/components/action_button.dart';
 import 'package:hearu/views/components/action_outlined_button.dart';
 
 class AnimatedActionButtons extends StatefulWidget {
@@ -15,30 +16,42 @@ class AnimatedActionButtons extends StatefulWidget {
 class _AnimatedActionButtonsState extends State<AnimatedActionButtons> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Flexible(
-          child: TextButton(
-            onPressed: () {
-              BlocProvider.of<OnboardingAnimationBloc>(context)
-                  .add(OnPreviousClick());
-            },
-            child: const Text("Skip", style: TextStyle(color: AppColors.dark)),
-          ),
-        ),
-        Flexible(
-          child: ActionOutlinedButton.withIcon(
-            onPressed: () {
-              BlocProvider.of<OnboardingAnimationBloc>(context)
-                  .add(OnNextClick());
-            },
-            trailingIcon: FontAwesomeIcons.arrowRightLong,
-            text: 'Next',
-          ),
-        )
-      ],
+    return BlocBuilder<OnboardingAnimationBloc, OnboardingAnimationState>(
+      builder: (context, state) {
+        if (state.currentIndex < 3) {
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: TextButton(
+                  onPressed: () {
+                    BlocProvider.of<OnboardingAnimationBloc>(context)
+                        .add(OnPreviousClick());
+                  },
+                  child: const Text("Skip",
+                      style: TextStyle(color: AppColors.dark)),
+                ),
+              ),
+              Flexible(
+                child: ActionOutlinedButton.withIcon(
+                  onPressed: () {
+                    BlocProvider.of<OnboardingAnimationBloc>(context)
+                        .add(OnNextClick());
+                  },
+                  trailingIcon: FontAwesomeIcons.arrowRightLong,
+                  text: 'Next',
+                ),
+              )
+            ],
+          );
+        } else {
+          return ActionButton.withIcon(
+              onPressed: () {},
+              trailingIcon: FontAwesomeIcons.rocket,
+              text: "Let's start!");
+        }
+      },
     );
   }
 }
